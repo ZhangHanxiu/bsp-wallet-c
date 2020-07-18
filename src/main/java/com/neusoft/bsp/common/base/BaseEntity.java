0476e6f59a610e55a975883d3cb54b32120c9318
ZhangHanxiu <1159577168@qq.com>
2020-07-19 00:45:04 +0800

    0719

--- src/main/java/com/neusoft/bsp/common/base/BaseEntity.java
+++
@@ -1,8 +0,0 @@
-package com.neusoft.bsp.common.base;
-
-
-import java.io.Serializable;
-
-public interface BaseEntity<E extends Serializable> extends Serializable {
-    E getId();
-}
--- src/main/java/com/neusoft/bsp/user/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.neusoft.bsp.user.mapper;
-
-import com.neusoft.bsp.common.base.BaseMapper;
-import com.neusoft.bsp.user.entity.User;
-
-public interface UserMapper extends BaseMapper<String, User> {
-}
